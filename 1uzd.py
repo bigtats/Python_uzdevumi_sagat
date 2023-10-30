8fe9aff4edb6008dd62a47a1cd007899d63f4fb1
Kaspars <97937777+bigtats@users.noreply.github.com>
2023-10-30 10:05:03 +0200

    Update 1uzd.py
    yeeeeeeee

--- 1uzd.py
+++ 1uzd.py
@@ -3,3 +3,34 @@ Uzrakstiet programmu definējot klasi,lai veselu skaitli pārveidotu par romieš
 
 """
 
+class AAA:
+    def __init__(self):
+        self.roman_numerals={
+            1:'I',
+            4:'IV',
+            5:'V',
+            9:'IX',
+            10:'X',
+            40:'XL',
+            50:'X',
+            90:'XC',
+            100:'C',
+            400:'CD',
+            500:'D',
+            900:'CM',
+            1000:'M',
+        }
+
+    def uz_roman(self, num):
+        result="" #Tukša virkne!
+        for value, numeral in sorted(self.roman_numerals.items(), key=lambda x: x[0], reverse=True):
+            while num>=value:
+                result+=numeral
+                num-=value
+        return result
+    
+#Piemērs
+skaitlis = 1984
+convert = AAA()
+roman_numeral = convert.to_roman(skaitlis)
+print(f"{skaitlis} romiešu ciparos ir {roman_numeral}")
